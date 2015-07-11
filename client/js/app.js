efae220af4eaff7dbbd83424930bc0615df4c17d
Jammie Mountz <jmmountz@gmail.com>
2015-07-10 17:27:13 -0700

    (dev setup) Added gulpfile and set up server to run from dist

---
+++ client/js/app.js
@@ -0,0 +1,4 @@
+var Connectus = require('./components/connectus');
+var React = require('react');
+
+React.render(<Connectus />, document.getElementById('main'));
---
+++ client/js/components/connectus.js
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var Connectus = React.createClass({
+  render: function() {
+    return <h1>ConnectUs!</h1>
+  }
+});
+
+module.exports = Connectus
