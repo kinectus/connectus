a31e9861b23510e49be3ff3c155b6b49716e4dc7
Jammie Mountz <jmmountz@gmail.com>
2015-07-11 16:43:30 -0700

    (outlet list) clicking outlet triggers event

--- client/js/stores/outletStore.js
+++ client/js/stores/outletStore.js
@@ -1,22 +1,24 @@
 var ConnectusDispatcher = require('../dispatcher/ConnectusDispatcher');
-// var ConnectusConstants = require('');
+// var ConnectusConstants = require('');          // omg what is this even
 var assign = require('react/lib/Object.assign');    // allows us to extend objects similarly to jquery and underscore lib...
 var EventEmitter = require('events').EventEmitter;
 
 var CHANGE_EVENT = 'change';
 
-var _outlets = {          // these will obviously actually come from the db when we get one
-  outlet1: {
+var _outlets = [          // these will obviously actually come from the db when we get one
+  { name: 'Jammies Outlet',
     color: 'black',
     type: 'high',
-    location: '944 Market St'
+    location: '944 Market St',
+    id: '4'
   },
-  outlet2: {
+  { name: 'Diannas Outlet',
     color: 'yellow',
     type: 'low',
-    location: '140 Hamerton Ave'
+    location: '140 Hamerton Ave',
+    id: '5'
   }
-};
+];
 
 var outletStore = assign({}, EventEmitter.prototype, {
   getOutlets: function() {
@@ -26,8 +28,14 @@ var outletStore = assign({}, EventEmitter.prototype, {
 
 ConnectusDispatcher.register(function(payload){
   var action = payload.action;
-  switch(action.actionType){
-    case outletConstant.MORE_INFO:
+  switch(action){
+    case 'CLICK_OUTLET':
+      _outlets.map(function(outlet){
+        if (outlet.id === payload.id){
+          outlet.color = 'red';
+          // trigger event!!
+        }
+      });
       break;
     default:
       return true;
