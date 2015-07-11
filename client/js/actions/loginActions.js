e70c913c31ed84d57b51bcc3b45c8040831c9cad
Valerie Liang <liang.val63@gmail.com>
2015-07-11 16:07:18 -0700

    updating

--- client/js/actions/loginActions.js
+++ client/js/actions/loginActions.js
@@ -4,27 +4,27 @@ var RouterContainer = require('../services/RouterContainer.js');
 
 var loginActions = {
   //check to see if user is already logged in - if not, set the token and reroute
-  loginUser: function(jwt){
-    var savedJwt = localStorage.getItem('token');
+  // loginUser: function(jwt){
+  //   var savedJwt = localStorage.getItem('token');
 
-    if(savedJwt !== jwt){
-      var nextPath = RouterContainer.get().getCurrentQuery().nextPath || '/';
+  //   if(savedJwt !== jwt){
+  //     var nextPath = RouterContainer.get().getCurrentQuery().nextPath || '/';
 
-      RouterContainer.get().transitionTo(nextPath);
-      localStorage.setItem('jwt', jwt);
-    }
+  //     RouterContainer.get().transitionTo(nextPath);
+  //     localStorage.setItem('jwt', jwt);
+  //   }
 
-    AppDispatcher.dispatch({
-      actionType: LOGIN_USER,
-      jwt: jwt
-    });
-  },
+  //   AppDispatcher.dispatch({
+  //     actionType: LOGIN_USER,
+  //     jwt: jwt
+  //   });
+  // },
 
-  logoutUser: function(){
-    RouterContainer.get().transitionTo('/login');
-    localStorage.removeItem('jwt');
-    AppDispatcher.dispatch({
-      actionType: LOGOUT_USER
-    });
-  }
+  // logoutUser: function(){
+  //   RouterContainer.get().transitionTo('/login');
+  //   localStorage.removeItem('jwt');
+  //   AppDispatcher.dispatch({
+  //     actionType: LOGOUT_USER
+  //   });
+  // }
 };
--- client/js/app.js
+++ client/js/app.js
@@ -1,5 +1,33 @@
 var ConnectusDispatcher = require('./dispatcher/ConnectusDispatcher');
 var React = require('react');
-var Connectus = require('./components/connectus')
+var Connectus = require('./components/connectus');
+var Router = {Route} = require('react-router');
+var AuthenticatedApp = require('./components/authentication.js');
+var Login = require('./components/login.js');
+var Signup = require('./components/signup.js');
+var LandingPage = require('./components/landingPage.js');
+var RouterContainer = require('./services/RouterContainer.js');
+var loginActions = require('./actions/loginActions.js');
 
-React.render(<Connectus />, document.getElementById('main'));
+var routes = (
+  <Route handler={AuthenticatedApp}>
+    <Route name="login" handler={Login}>
+    <Route name="signup" handler={Signup}>
+    <Route name="landingPage" path="/" handler={LandingPage}>
+  </Route>
+);
+
+var router = Router.create({routes});
+RouterContainer.set(router);
+
+var jwt = localStorage.getItem('jwt');
+if(jwt){
+  LoginActions.loginUser(jwt);
+}
+
+router.run(function(Handler){
+  React.render(<Handler />, document.getElementbyId('main'));
+})
+
+
+// React.render(<Connectus />, document.getElementById('main'));
