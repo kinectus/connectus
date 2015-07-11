7f41e932397ca5cd67fe61b7b9d42a8a5c8db5bc
Dianna Faulk <diannafaulk@gmail.com>
2015-07-11 12:16:46 -0700

    (database) Add outlets table

--- server/config/config.js
+++ server/config/config.js
@@ -14,21 +14,43 @@ var db = require('knex')({
     // filename: path.join(__dirname, './kinectus/kinectus.mysql/')
   }
 });
-// 
+// db.schema.raw('DROP SCHEMA IF EXISTS connectus; CREATE SCHEMA IF NOT EXISTS connectus; USE connectus');
 var Bookshelf = require('bookshelf')(db);
 // Add users table to db, store authentication
 db.schema.hasTable('users').then(function(exists){
-  console.log("INSIDE")
-  console.log(exists);
+  // For design process ease, drop tables
+  // if(exists){
+  //   db.schema.dropTable('users');
+  //   console.log('dropped')
+  // }
+  console.log(exists)
   if(!exists){
     db.schema.createTable('users', function(user){
       user.increments('id').primary();
       user.string('username', 20);
-      user.timestamps();
+      // user.timestamps();
     }).then(function(table){
       console.log('Created users table', table);
     }); 
   }
 });
 
-module.exports = db;
+db.schema.hasTable('outlets').then(function(exists){
+  // For design process ease, drop tables
+  // if(exists){
+  //   db.schema.dropTable('outlets');
+  //   console.log('dropped')
+  // }
+  if(!exists){
+    db.schema.createTable('outlets', function(outlet){
+      outlet.increments('id').primary();
+      outlet.string('name', 30);
+      // outlet.foreignkey('seller', )
+      // outlet.timestamps();
+    }).then(function(table){
+      console.log('Created outlets table', table);
+    }); 
+  }
+});
+
+module.exports = Bookshelf;
