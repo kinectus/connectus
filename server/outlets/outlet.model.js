cea53d7fa9605311e12e422bb33af7f5c8d35e4f
Dianna Faulk <diannafaulk@gmail.com>
2015-07-11 15:37:03 -0700

    (fix) Update relationships between users, transactions and outlets

--- server/outlets/outlet.model.js
+++ server/outlets/outlet.model.js
@@ -1,12 +1,13 @@
 var bookshelf = require('../config/config');
+var User = require('../users/user.model');
 
 var Outlet = bookshelf.Model.extend({
   tableName: 'outlets',
   seller: function() {
-    return this.belongsTo(User); // trying to find converse in User model
+    return this.belongsTo(User, 'seller_id'); // trying to find converse in User model
   },
   buyer: function() {
-    return this.belongsTo(User);
+    return this.belongsTo(User, 'buyer_id');
   }
 });
 
--- server/users/user.model.js
+++ server/users/user.model.js
@@ -1,10 +1,11 @@
 var bookshelf = require('../config/config');
+var Outlet = require('../outlets/outlet.model');
 
 var User = bookshelf.Model.extend({
   tableName: 'users'
 });
 
-// User.hasMany('Outlet');
-// User.hasMany('Transaction');
+User.hasMany('Outlet');
+User.hasMany('Transaction');
 
 module.exports = User;
