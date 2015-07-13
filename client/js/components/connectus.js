2f8caeed711a1aece0f361236ecc184cd3041e88
Valerie Liang <liang.val63@gmail.com>
2015-07-13 11:28:32 -0700

    basic routing built in

--- client/js/components/connectus.js
+++ client/js/components/connectus.js
@@ -1,46 +1,15 @@
 var React = require('react');
-var outletStore = require('../stores/outletStore');
-var ConnectusDispatcher = require('../dispatcher/ConnectusDispatcher');
+var RouteHandler = require('react-router').RouteHandler;
 
 var Connectus = React.createClass({
-
-  getInitialState: function(){
-    console.log('initial state gotten by component')
-
-    return {
-      list: outletStore.getOutlets()
-    }
-  },
-
-  showOutletInfo: function(id){
-    ConnectusDispatcher.dispatch({
-        action: 'CLICK_OUTLET',
-        id: id
-    });
-  },
-
-  outletInfoChange: function(){
-    this.forceUpdate;
-
-  },
-
-  render: function() {
-    var that = this;
-    var outlets = outletStore.getOutlets();
-
-    var outletHtml = outlets.map( function( outlet ) {
-      return <li data-tag={outlet.id} onClick={that.showOutletInfo.bind(null, outlet.id)} key={ outlet.id }>
-        { outlet.name } { outlet.color }
-      </li>;
-    });
-
-    return <div>
-      <ul>
-          { outletHtml }
-      </ul>
-    </div>;
+  render: function(){
+    return (
+	    <div>
+	      <h1>Connect us!</h1>
+	      <div>
+	        <RouteHandler />
+	      </div>
+	    </div>
+    )
   }
-
 });
-
-module.exports = Connectus
--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 //added
 var app = require('./config/middleware');
-var bodyParser = require('body-parser');
 
 var port = process.env.PORT || 3000;
 // var db = require('./db/config.js');
