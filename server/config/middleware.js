7b6f6d1fcb281e737519f65ce729aacb1cdebb2c
Jammie Mountz <jmmountz@gmail.com>
2015-07-13 12:01:11 -0700

    (bug fix) server dist file structure changed

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -5,14 +5,7 @@ var db = require('./db/config');
 require('../auth/auth.controller');
 // include morgan, 
 
-module.exports = function() {
-  var app = express();
-  //app.use(morgan('dev'));
-  //app.use(bodyParser.json());
-  app.use(express.static( path.join( __dirname + '/../dist') ) );
+var app = express();
+app.use(express.static(path.join( __dirname + '/../../dist')));
 
-  // put these before routes.  order matters!
-  app.use(passport.initialize());
-  app.use(passport.session());
-  return app;
-}
+module.exports = app;
