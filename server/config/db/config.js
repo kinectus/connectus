7c5378632bb99c474291904409744d252a304453
Sean Connor <seantconnor@gmail.com>
2015-07-14 16:07:26 -0700

    db url

--- server/config/db/config.js
+++ server/config/db/config.js
@@ -6,7 +6,7 @@ var outletExamples = require('./outletDataExamples')
 var db = require('knex')({
   client: 'mysql',
   connection: {
-    host: 'us-cdbr-iron-east-02.cleardb.net/heroku_09dd1820db30399?reconnect=true',//mysql://bd88d20b6f0b67:7e80e308@
+    host: 'mysql://bd88d20b6f0b67:7e80e308@us-cdbr-iron-east-02.cleardb.net/heroku_09dd1820db30399?reconnect=true',//mysql://bd88d20b6f0b67:7e80e308@
     port: '',
     user: 'bd88d20b6f0b67',
     password: '7e80e308',
