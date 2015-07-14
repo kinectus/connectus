a2d6f3f451f670ddd20c10cc5928a0717ad65e2a
Dianna Faulk <diannafaulk@gmail.com>
2015-07-14 15:39:43 -0700

    (powerReader) Asynchronously handle energy data and update total

--- server/config/powerReader.js
+++ server/config/powerReader.js
@@ -14,25 +14,9 @@ var make = require('shelljs/make');
 var fs = require('fs');
 var watch = require('node-watch');
 
-// COMMENTS ON GRABBING LAST 7 LINES
-var readFile = function(cb){
-  fs.readFile('data.txt', 'utf-8', function(err, data){
-    if (err){
-      console.log('err', err);
-    } else {
-      // console.log('data',data);
-      // Grab last 7 lines
-      // var section = 
-      var arr = data.split('\n');
-      line = arr[arr.length-3];
-      cb(line);
-      console.log('split', arr[arr.length-3]);
-    }
-  })
-};
-
+// Total wattage since start
 var total = 0;
-
+// Grab wattage information from string
 var getWatts = function(string){
   var start = string.indexOf(': ')+2;
   var end = string.indexOf('w');
@@ -41,27 +25,74 @@ var getWatts = function(string){
   if (end > 0){
     total += watts;
   }
-  console.log('total: ', total, 'start: ', start, 'end: ', end);
+  // console.log('string: ', string, ' total: ', total, ' start: ', start, ' end: ', end, ' watts: ', watts);
+  // console.log('total: ', total);
+  cbDone = true;
 }
 
-// CHANGED TO APPEND TO DATA.TXT
-setInterval(shell.exec, 10000, "hacklet read -n 0x2777 -s 0 >> data.txt | cat");
+var cbDone = false;
+var input;
 
-watch('data.txt', { recursive: false }, function(){ readFile(getWatts); });
-//readFile(getWatts);
+// Executes hardware query on CL > writes result to data.txt > initiates readLines
+var execute = function(command){
+  shell.exec.apply(null, [command]);
+  input = fs.createReadStream('data.txt', 'utf8');
+  readLines(input, getWatts);
+};
 
+// Runs execute every 10s
+setInterval(execute, 10000, "hacklet read -n 0x2777 -s 0 >> data.txt | cat");
 
-// setInterval(function(){
+// Executes callback with data.txt and then erases data.txt when done
+var readLines = function(input, cb) {
+  var remaining = '';
+
+  input.on('data', function(data) {
+    remaining += data;
+    var index = remaining.indexOf('\n');
+    var last  = 0;
+    while (index > -1) {
+      var line = remaining.substring(last, index);
+      var length = line.length;
+      if (length > 74){
+        cb(line);
+      }
+      last = index + 1;
+      index = remaining.indexOf('\n', last);
+    }
+    remaining = remaining.substring(last);
+  });
+
+  input.on('end', function() {
+    if (remaining.length > 0) {
+      cb(remaining);
+    }
+    fs.writeFile('data.txt', '', function(){console.log('ERASED DATA.TXT')});
+  });
+};
+
+// Commented code contains parts of previous strategy without data erase
+// // COMMENTS ON GRABBING LAST 7 LINES
+// var readFile = function(cb){
 //   fs.readFile('data.txt', 'utf-8', function(err, data){
 //     if (err){
 //       console.log('err', err);
 //     } else {
 //       // console.log('data',data);
+//       // Grab last 7 lines
+//       // var section = 
 //       var arr = data.split('\n');
-//       getWatts(arr[arr.length-3]);
+//       line = arr[arr.length-3];
+//       cb(line);
 //       console.log('split', arr[arr.length-3]);
 //     }
 //   })
-// }, 10000);
+// };
+
+//fs.writeFile('/path/to/file', '', function(){console.log('done')})
+//{ recursive: false },
+
+// watch('data.txt', { recursive: false }, function(){ readFile(getWatts); });
+
 
 // split I, [2015-07-13T20:06:15.137579 #84220]  INFO -- : 45w at 2015-07-13 20:06:01 -0700
