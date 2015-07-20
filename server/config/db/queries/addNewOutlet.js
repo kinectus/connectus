a9610ec43303f47d53a29e3aaa778d1c7c03848a
Dianna Faulk <diannafaulk@gmail.com>
2015-07-20 15:07:21 -0700

    (refactor) Move database queries to query folder

---
+++ server/config/db/queries/addNewOutlet.js
@@ -0,0 +1,56 @@
+var db = require('../config');
+var Outlet = require('../../../outlets/outlet.model');
+var User = require('../../../users/user.model');
+var addReservations = require('./addReservationSlots');
+
+module.exports = addNewOutlet = function(req, res){
+  var data = req.body;
+
+  new User({
+    username: req.user.id
+  }).fetch().then(function(user){
+    new Outlet({
+      name: data.name,
+      priceEnergy: data.charge,
+      // priceHourly: data.priceHourly,
+      // lat: data.lat,
+      // long: data.long,
+      description: data.description,
+      // priceSuggest: data.priceSuggest,
+      address: data.address,
+      voltage: data.voltage
+    }).fetch().then(function(found) {
+      if (found) {
+        res.status(200).send(found.attributes);
+      } else {
+        var outlet = new Outlet({
+          name: data.name,
+          priceEnergy: data.charge,
+          seller_id: user.id,
+          // priceHourly: data.priceHourly,
+          // lat: data.lat,
+          // long: data.long,
+          description: data.description,
+          // priceSuggest: data.priceSuggest,
+          address: data.address,
+          voltage: data.voltage
+        });
+
+        outlet.save().then(function(newOutlet){
+          console.log('newOutlet before addReservations: ', newOutlet.attributes);
+          addReservations(newOutlet.attributes);
+          return newOutlet;
+        })
+        .then(function(newOutlet){
+          res.status(200).send(newOutlet);
+        })
+        .catch(function(error){
+          console.log(error);
+        });
+      }
+    });
+  })
+  .catch(function(error){
+    console.log(error);
+  });
+}
