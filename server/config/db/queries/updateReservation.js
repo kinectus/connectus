816d22b8dbe774bbfc66aa069909a411f0479fc2
Dianna Faulk <diannafaulk@gmail.com>
2015-07-21 16:15:05 -0700

    (refactor) Change server response format

--- server/config/db/queries/updateReservation.js
+++ server/config/db/queries/updateReservation.js
@@ -16,11 +16,6 @@ module.exports = updateReservation = function(req, res){
   var sameDay = (startDate === endDate);
   // Transaction tracking
   var transactionID;
-  // Response
-  var status, message;
-  if (status && message){
-    return res.status(status).send(message);
-  }
 
   // Recursive function updates reservation slots and creates transaction models for each
   // time slot from beginning to end 
@@ -45,14 +40,12 @@ module.exports = updateReservation = function(req, res){
       // Update reservation
       .then(function(newReservation){
         if (!newReservation) {
-          // res.status(404).send('No open reservation found');
-          status = 404;
-          message = 'No open reservation found';
+          console.log('sending res 404 no slot');
+          res.send(404, 'No slot found');
           return;
-        } else if (!newReservation.available) {
-          // res.status(404).send('Reservation is not available: ', newReservation);
-          status = 404;
-          message = 'Reservation is not available'
+        } else if (newReservation.available == false) {
+          console.log('sending res 404 not available');
+          res.send(404, 'Reservation is not available');
           return;
         } else {
           newReservation.set({
@@ -68,11 +61,10 @@ module.exports = updateReservation = function(req, res){
         // Completion check for same day reservations
         if (sameDay){
           if (++currentSlot <= endSlot){
-            newReservation(user, currentSlot);
+            return newReservation(user, currentSlot);
           } else {
-            // res.status(201).send('POST reservations complete');
-            status = 201;
-            message = 'POST reservations complete';
+            console.log('sending res 201 sameday posted');
+            res.send(201, 'POST reservations complete');
             return;
           }
         // Completion check for multi-day reservations
@@ -83,11 +75,10 @@ module.exports = updateReservation = function(req, res){
           console.log('startDate: ', startDate, ', endDate: ', endDate, ', currentDate: ', currentDate, ', difference: ', difference);
           if ( difference <= 0 ){
             if ( currentSlot <= endSlot  || difference < 0){
-              newReservation(user, currentSlot);
+              return newReservation(user, currentSlot);
             } else {
-              // res.status(201).send('POST reservations complete');
-              status = 201;
-              message = 'POST reservations complete';
+              console.log('sending res 201 multi-day posted');
+              res.send(201, 'POST reservations complete');
               return;
             }
           }
@@ -99,8 +90,7 @@ module.exports = updateReservation = function(req, res){
 
   // START RESERVATION PROCESS
   // Fetch user by request user id
-
-  return new User({
+  new User({
     username: req.user.id
   }).fetch().then(function(user){
     // Find start timeSlot
@@ -115,7 +105,7 @@ module.exports = updateReservation = function(req, res){
       // Start making reservations for user
       .then(function(slot2){
         endSlot = slot2.id;
-        newReservation(user.id, startSlot)
+        newReservation(user.id, startSlot);
       });
     });
   });
