a92f7e7c2b46b37e3d5d698a993572d315517358
Dianna Faulk <diannafaulk@gmail.com>
2015-07-24 20:18:49 -0700

    (editOutlet) Update existing outlet with new information on submit

--- server/config/db/queries/updateOutlet.js
+++ server/config/db/queries/updateOutlet.js
@@ -4,53 +4,24 @@ var User = require('../../../users/user.model');
 var addReservations = require('./addReservationSlots');
 
 module.exports = addNewOutlet = function(req, res){
+
   var data = req.body;
-  console.log('IN UPDATE OUTLET');
 
-  res.send(201, 'made it to back!');
-  // new Outlet({
-  //   name: data.name,
-  //   priceEnergy: data.charge,
-  //   // priceHourly: data.priceHourly,
-  //   // lat: data.lat,
-  //   // long: data.long,
-  //   description: data.description,
-  //   // priceSuggest: data.priceSuggest,
-  //   address: data.address,
-  //   voltage: data.voltage
-  // }).fetch().then(function(found) {
-  //   if (found) {
-  //     res.send(201, found.attributes);
-  //   } else {
-  //     var outlet = new Outlet({
-  //       name: data.name,
-  //       priceEnergy: data.charge,
-  //       seller_id: user.id,
-  //       // priceHourly: data.priceHourly,
-  //       // lat: data.lat,
-  //       // long: data.long,
-  //       description: data.description,
-  //       // priceSuggest: data.priceSuggest,
-  //       address: data.address,
-  //       voltage: data.voltage,
-  //       thumbs_up: 0,
-  //       thumbs_down: 0
-  //     });
+  return new Outlet()
+  .query({ where: {id: data.id} })
+  .fetch()
+  .then(function(newOutlet) {
+    console.log(newOutlet);
+    newOutlet.set({
+      address: data.address,
+      name: data.name,
+      description: data.description,
+      voltage: data.voltage,
+      priceEnergy: data.charge
+    }).save()
+    .then(function(newOutlet){
+      res.send(201, newOutlet);
+    })
+  });
 
-  //     outlet.save().then(function(newOutlet){
-  //       console.log('newOutlet before addReservations: ', newOutlet.attributes);
-  //       addReservations(newOutlet.attributes);
-  //       return newOutlet;
-  //     })
-  //     .then(function(newOutlet){
-  //       res.send(200, newOutlet);
-  //     })
-  //     .catch(function(error){
-  //       console.log(error);
-  //     });
-  //   }
-  // })
-  // .catch(function(error){
-  //   console.log(error);
-  // });
-}
+};
