aa6992f648fb4bcd2be1a67d041e8f4d87526e0c
Jammie Mountz <jmmountz@gmail.com>
2015-07-27 19:35:15 -0700

    (splash page) added animation

--- client/js/components/about.js
+++ client/js/components/about.js
@@ -5,11 +5,17 @@ var Link = require('react-router').Link;
 // var ReactAddons = require('react/addons');
 
 var About = React.createClass({
+
+  componentDidMount: function() {
+  },
+
   render: function(){
     return (
   	  <div className="about">
         <div className="header-about">
-          <div className="text"><p>Charge anything anywhere.</p></div>
+          <div className="header-text">
+            <div className="banner"><span>Charge anything anywhere.</span></div>
+          </div>
         </div>
           <div className="section howitworks">
 		          <h1>How does Connectus work?</h1>
@@ -19,7 +25,6 @@ var About = React.createClass({
 		          <h1>What energy looks like today</h1>
 			        <h2>This is how Connectus plays into the bigger picture</h2>
           </div>
-          
       </div>
     );
   }
