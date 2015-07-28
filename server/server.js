5172046e32a8a0c90b266a0440eaf2d62124b07b
Sean Connor <seantconnor@gmail.com>
2015-07-27 21:57:10 -0700

    updating DB with realtime transaction data. need to add stub data fro transaction

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var app = require('./config/middleware');
 
 var setRealtimeData= require('./config/db/queries/setRealtimeTransactionData');
+
 var port = process.env.PORT || 3000;
 // var db = require('./db/config.js');
 
@@ -17,7 +18,9 @@ io.on('connection', function(socket){
 
 app.post('/realtimeData', function(req, res){
   console.log('in the realtimeData post', req.body)
-  mySocket.emit('energy', req.body)
+  mySocket.emit('energy', req.body);
+  // add to DB
+  setRealtimeData(req.body);
 })
 
 console.log('Connectus is listening on port ' + port + '...');
