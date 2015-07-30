e54a92035d175f0e559638ba5d873c1dfd250a16
Valerie Liang <liang.val63@gmail.com>
2015-07-30 15:41:17 -0700

    removed login and signup pages

--- client/js/config/routes.js
+++ client/js/config/routes.js
@@ -1,8 +1,6 @@
 //client-side routing
 var React = require('react');
 var Connectus = require('../components/connectus.js');
-var Signup = require('../components/signup.js');
-var Login = require('../components/login.js');
 var LandingPage = require('../components/landingPage.js');
 var About = require('../components/about.js');
 var OutletsList = require('../components/outletsList.js');
@@ -25,8 +23,6 @@ var Route = Router.Route;
 //connectus - always shown on the home page - holdes a <RouteHandler> that displays the other compnents
 module.exports = (
   <Route name="connectus" path="/" handler={Connectus} > 
-    <Route name="login" path="/login" handler={Login} />
-    <Route name="signup" path="/signup" handler={Signup} />
     <Route name="about" path="/about" handler={About} />
     <Route name="landingPage" path="/landingPage" handler={LandingPage} />
     <Route name="paymentsPage" path="/paymentsPage" handler={PaymentsPage} />
