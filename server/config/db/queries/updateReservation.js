b74dd9b08d0e04bea043e4fef08cb461b84cbc2e
Dianna Faulk <diannafaulk@gmail.com>
2015-07-30 19:41:10 -0700

    (updateReservation) Change id search to account for clearDB 10 step increment

--- server/config/db/queries/updateReservation.js
+++ server/config/db/queries/updateReservation.js
@@ -8,77 +8,79 @@ var transactionID;
 
 module.exports = updateReservation = function(req, res){
   var data = req.body;
-  // Slot tracking
-  var startSlot, endSlot, currentSlot;
-  // Date tracking
-  var startDate = data.start.date;
+
+  // Date and time tracking, completion tracking
+  var currentDate = data.start.date;
   var endDate = data.end.date;
-  var currentDate;
-  var sameDay = (startDate === endDate);
+  var currentSlot, endSlot;
   var complete = false;
-  var is404 = false;
+  // Determine id step for clearDB
+  if (process.env.NODE_ENV === 'development'){
+    var min = 1;
+    var max = 48;
+    var step = 10;
+  } else {
+    var min = 2;
+    var max = 472;
+    var step = 10;
+  }
 
   // Recursive function updates reservation slots and creates transaction models for each
   // time slot from beginning to end 
   var newReservation = function(user, passedSlot){
-    currentSlot = passedSlot;
     currentDate = currentDate || startDate;
 
-      // Find reservation
-      new Reservation()
-      .query({where: {outlet_id: data.outletID, slot_id: currentSlot, date: currentDate} })
-      .fetch()
+    // Find reservation
+    new Reservation()
+    .query({where: {outlet_id: data.outletID, slot_id: passedSlot, date: currentDate} })
+    .fetch()
 
-      // Update reservation
-      .then(function(newReservation){
-        if (!newReservation) {
-          is404 = true;
+    // Update reservation
+    .then(function(newReservation){
+      if (!newReservation) {
+        is404 = true;
+        complete = true;
+        return;
+      } else if (newReservation.available == false) {
+        is404 = true;
+        complete = true;
+        return;
+      } else {
+        newReservation.set({
+          buyer_id: user,
+          available: false,
+          transaction_id: transactionID
+        }).save();
+      }
+    })
+    // Determine if more reservations need to be updated
+    .then(function(){
+      // Update slot and day (if necessary)
+      // Determine if reservations should continue
+
+      currentSlot = currentSlot < max ? currentSlot += step : min;
+      currentDate = currentSlot === min ? moment(currentDate).add(1, 'days').format('YYYY-MM-DD').toString() : currentDate;
+      var difference = moment(currentDate).diff(moment(endDate));
+      // console.log('startDate: ', startDate, ', endDate: ', endDate, ', currentDate: ', currentDate, ', difference: ', difference);
+      console.log('currentSlot: ', currentSlot, 'currentDate', currentDate)
+      if ( difference <= 0 ){
+        if ( currentSlot <= endSlot  || difference < 0){
+          return newReservation(user, currentSlot);
+        } else {
           complete = true;
           return;
-        } else if (newReservation.available == false) {
-          is404 = true;
-          complete = true;
-          return;
-        } else {
-          newReservation.set({
-            buyer_id: user,
-            available: false,
-            transaction_id: transactionID
-          }).save();
         }
-      })
-      // Determine if more reservations need to be updated
-      .then(function(){
-        // Completion check for same day reservations
-        if (sameDay){
-          if (++currentSlot <= endSlot){
-            return newReservation(user, currentSlot);
-          } else {
-            complete = true;
-            return;
-          }
-        // Completion check for multi-day reservations
-        } else {
-          currentSlot = currentSlot < 48 ? ++currentSlot : 1;
-          currentDate = currentSlot === 1 ? moment(currentDate).add(1, 'days').format('YYYY-MM-DD').toString() : currentDate;
-          var difference = moment(currentDate).diff(moment(endDate));
-          console.log('startDate: ', startDate, ', endDate: ', endDate, ', currentDate: ', currentDate, ', difference: ', difference);
-          if ( difference <= 0 ){
-            if ( currentSlot <= endSlot  || difference < 0){
-              return newReservation(user, currentSlot);
-            } else {
-              complete = true;
-              return;
-            }
-          }
-        }
-      }).then(function(){
-        if (complete === true && !is404){
-          return res.send(201, JSON.stringify('Reservation complete'));
-        } else if (complete === true && is404) {
-          return res.send(404, JSON.stringify('Cannot complete reservation'));
-        }
-      });
+      }
+    })
+    // Send response if complete
+    .then(function(){
+      if (complete === true && !is404){
+        return res.send(201, JSON.stringify('Reservation complete'));
+      } else if (complete === true && is404) {
+        return res.send(404, JSON.stringify('Cannot complete reservation'));
+      }
+    });
+
   };
 
 
@@ -93,7 +95,7 @@ module.exports = updateReservation = function(req, res){
       start: data.start.time
     }).fetch()
     .then(function(slot){
-      startSlot = slot.id;
+      currentSlot = slot.id;
       // Find end timeslot
       new TimeSlot({
         end: data.end.time
@@ -112,7 +114,7 @@ module.exports = updateReservation = function(req, res){
         .then(function(newTransaction){
           transactionID = newTransaction.id;
           complete = false;
-          return newReservation(user.id, startSlot);
+          return newReservation(user.id, currentSlot);
         });
       });
     });
