9e224977fef24f218fa0eb352f1a95b2045bb3e8
Dianna Faulk <diannafaulk@gmail.com>
2015-09-30 19:56:18 -0700

    (refactor) Move Availability React component and Viewer component to own files

---
+++ client/js/components/availability/viewer.js
@@ -0,0 +1,51 @@
+var React = require('react');
+var Availability = require('./availability');
+
+// Contains buttons, passes their events to Availability
+var Viewer = React.createClass({
+  getInitialState: function(){
+    return { 
+      mouseDown: false,
+      forward: false,
+      move: false
+    }
+  },
+
+  // On forward mouse hold
+  mouseDownForward: function(){
+    this.setState({mouseDown: true});
+    this.setState({forward: true});
+    this.setState({move: true});
+  },
+
+  // On forward mouse hold
+  mouseDownBack: function(){
+    this.setState({mouseDown: true});
+    this.setState({forward: false});
+    this.setState({move: true});
+  },
+
+  // On forward mouse release
+  mouseUp: function(){
+    this.setState({mouseDown: false});
+    this.setState({move: false});
+  },
+
+  // Render buttons, pass states to Availability
+  render: function(){
+    return (
+      <div className="timeblock holder centering">
+        <div className="centering">
+          <Availability move={this.state.move} mouseDown={this.state.mouseDown} forward={this.state.forward} outletID = {this.props.outletID}/>
+        </div>
+        <div className="centering pad-top">
+          <button className="btn btn-default toggle glyphicon glyphicon-chevron-left" onMouseDown={this.mouseDownBack} onMouseUp={this.mouseUp} onMouseLeave={this.mouseUp}></button>
+          <button className="btn btn-default toggle glyphicon glyphicon-chevron-right" onMouseDown={this.mouseDownForward} onMouseUp={this.mouseUp} onMouseLeave={this.mouseUp}></button>
+        </div>
+      </div>
+    )
+  }
+
+});
+
+module.exports = Viewer;
